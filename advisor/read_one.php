5aad4e7fc21b3fe89529da23996f69747ac69dc6
barissokat <barissokat@gmail.com>
2019-01-11 16:01:41 +0300

    folder that has advisor processes

---
+++ advisor/read_one.php
@@ -0,0 +1,49 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+ 
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/advisor.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// prepare advisor object
+$advisor = new Advisor($db);
+ 
+// set ID property of record to read
+$advisor->id = isset($_GET['id']) ? $_GET['id'] : die();
+ 
+// read the details of advisor to be edited
+$advisor->readOne();
+ 
+if($advisor->name!=null){
+    // create array
+    $advisor_arr = array(
+        "id" =>  $advisor->id,
+        "name" => $advisor->name,
+        "area" => $advisor->area
+ 
+    );
+ 
+    // set response code - 200 OK
+    http_response_code(200);
+ 
+    // make it json format
+    echo json_encode($advisor_arr);
+}
+ 
+else{
+    // set response code - 404 Not found
+    http_response_code(404);
+ 
+    // tell the user advisor does not exist
+    echo json_encode(array("message" => "Advisor does not exist."));
+}
+?>
---
+++ advisor/read_paging.php
@@ -0,0 +1,74 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+ 
+// include database and object files
+include_once '../config/core.php';
+include_once '../shared/utilities.php';
+include_once '../config/database.php';
+include_once '../objects/advisor.php';
+ 
+// utilities
+$utilities = new Utilities();
+ 
+// instantiate database and advisor object
+$database = new Database();
+$db = $database->getConnection();
+ 
+// initialize object
+$advisor = new Advisor($db);
+ 
+// query advisors
+$stmt = $advisor->readPaging($from_record_num, $records_per_page);
+$num = $stmt->rowCount();
+ 
+// check if more than 0 record found
+if($num>0){
+ 
+    // advisors array
+    $advisors_arr=array();
+    $advisors_arr["records"]=array();
+    $advisors_arr["paging"]=array();
+ 
+    // retrieve our table contents
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+ 
+        $advisor_item=array(
+            "id" => $id,
+            "name" => $name,
+            "area" => html_entity_decode($area)
+        );
+ 
+        array_push($advisors_arr["records"], $advisor_item);
+    }
+ 
+ 
+    // include paging
+    $total_rows=$advisor->count();
+    $page_url="{$home_url}advisor/read_paging.php?";
+    $paging=$utilities->getPaging($page, $total_rows, $records_per_page, $page_url);
+    $advisors_arr["paging"]=$paging;
+ 
+    // set response code - 200 OK
+    http_response_code(200);
+ 
+    // make it json format
+    echo json_encode($advisors_arr);
+}
+ 
+else{
+ 
+    // set response code - 404 Not found
+    http_response_code(404);
+ 
+    // tell the user advisors does not exist
+    echo json_encode(
+        array("message" => "No advisors found.")
+    );
+}
+?>
